76a07f1fc45a6e800f35964b01f98d16c333e61f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-21 15:48:11 +0000

    Deploy Dollychhabra/hacker-news to github.com/Dollychhabra/hacker-news.git:gh-pages

---
+++ precache-manifest.e41f458520e06d8602d2a2dd4bd32e63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84259341dd56388a38a8419866311639",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10e8f0cd6f91b66444c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b1960e15f2d95d6bf73",
+    "url": "/static/js/2.8076474c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8076474c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10e8f0cd6f91b66444c",
+    "url": "/static/js/main.67401b13.chunk.js"
+  },
+  {
+    "revision": "407a98440c279500d425",
+    "url": "/static/js/runtime-main.a5e85371.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
