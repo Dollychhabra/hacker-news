fcc42fe4900978ecf7d495cfb0552ff85c23d849
dolchhab <dolly.chhabra@publicissapient.com>
2020-05-26 18:18:20 +0530

    integration ofSSR in progress

--- src/sagas.js
+++ src/sagas.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 
-import { homePageSaga } from './containers/organisms/HomePage/sagas';
+import homePageSaga from './containers/organisms/HomePage/sagas';
 import voteSaga from './containers/atoms/UpVote/sagas';
 
 export default function* rootSaga() {
