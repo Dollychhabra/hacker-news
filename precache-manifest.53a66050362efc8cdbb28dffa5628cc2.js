49f6e52407702aa691cf7401d6ef885fc02c0084
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-26 13:05:17 +0000

    Deploy Dollychhabra/hacker-news to github.com/Dollychhabra/hacker-news.git:gh-pages

---
+++ precache-manifest.53a66050362efc8cdbb28dffa5628cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db33b2d1d9ff3d0d562f857ad066adc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4412b8759c1d33e8e553",
+    "url": "/static/css/main.78f26dd2.chunk.css"
+  },
+  {
+    "revision": "ca44603cb6f9fa82862b",
+    "url": "/static/js/2.4b086949.chunk.js"
+  },
+  {
+    "revision": "8ced3e7d2377db1986992934dc6262e2",
+    "url": "/static/js/2.4b086949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4412b8759c1d33e8e553",
+    "url": "/static/js/main.654d3978.chunk.js"
+  },
+  {
+    "revision": "eb92d276d0041e55efa3",
+    "url": "/static/js/runtime-main.37f0ff04.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60831815dbd7432afab12f1c6ec57125.js"
+  "/precache-manifest.53a66050362efc8cdbb28dffa5628cc2.js"
 );
 
 self.addEventListener('message', (event) => {
